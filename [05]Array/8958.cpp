4d07a4716c325a9e07e055f78343c049f4bc5669
limjustin <limjyjustin@gmail.com>
2021-05-30 12:07:11 +0000

    20210530

--- [05]Array/8958.cpp
+++ [05]Array/8958.cpp
@@ -2,38 +2,63 @@
 #include <vector>
 using namespace std;
 
-struct TEST_CASE {
-  char test_case[80];
-};
-
 int main(void) {
-  // Input N
-  int N;
-  cin >> N;
+  // Input number of test case
+  int T;
+  cin >> T;
   
-  // Make test case vector
-  vector<TEST_CASE> tc_vector;
-  TEST_CASE tc;
+  // Input each test cases
+  vector<string> quiz; // quiz array size is same as for-loop count
+  char testcase[80];
   
-  for(int i = 0; i < N; i++) {
-    // cin >> test_case; // 이거 공부하기!!
-    scanf("%s", tc.test_case);
-    
-    tc_vector.push_back(tc);
-  
-    printf("%s", tc_vector[i]);
-    
+  for(int i = 0; i < T; i++) {
+    scanf("%s", testcase);
+    quiz.push_back(testcase);
   }
   
-  for(int i = 0; i < N; i++) {
+  // Get score of each quiz
+  for(int i = 0; i < T; i++) {
     
+    int cnt = 0;
+    int result = 0;
+    
+    for(int j = 0; j < quiz[i].size(); j++) {
+      
+      // 'X'라면 당연히 카운트는 0으로 다시 초기화
+      if(quiz[i][j] == 'X')
+        cnt = 0;
+      
+      // 카운트를 1 올리고 넘어가는 방식
+      else if(quiz[i][j] == 'O')
+        cnt = cnt + 1;
+      
+      result += cnt;
+    }
+    
+    cout << result << "\n"; // 여기서도 굳이 결과 벡터 만들이유 X
   }
+
 }
 
-// vector로 문자열을 받을 수 있냐?
-// 처음 벡터를 만들고 문자열을 받고 push_back 하면 될 듯
-// 카운트를 세는데 이전이 1 이상이면 1에다가 누적 카운트
+/*
+<처음에 헤매었던 점>
+1. 문자열들을 어떻게 담을 것인가?
+-> 문제 : 문자열 vector는 없나? 2차원 배열을 vector로 어떻게 구현하지?
+-> 해결 : String vector 안에 char 배열 push_back
+          근데 이게 제일 깔끔할지는 잘 머르겠음
+-> 예제 :   vector<string> str;
+            char arr1[20] = "Hello World";
+            str.push_back(arr1);
+            char arr2[20] = "Welcome Seoul";
+            str.push_back(arr2);
+            cout << str[1][2] << "\n";
+                    
+-> 문제 : 문자열을 cin으로 어떻게 받을 거야?
+-> 해결 : 배열 쓰니까 해결(scanf)
 
-// 앞으로 취할 방향
-// 1. 문자열 2차원 배열 어케 받을지
-// 2. 받을 때 scanf or cin
+
+2. 연속된 'O'표 어떻게 처리할 것인가? 
+-> 해결 : 누적변수 사용
+          'O'표가 나오면 계속 누적해서 그 값을 더하고,
+          'X'표가 나오면 누적된 값 초기화해서 다시 시작
+*/
