1f8899e8eb911600ac7526ebb50991cec94be8ea
limjustin <limjyjustin@gmail.com>
2021-05-31 11:00:52 +0000

    20210531

--- [07]String/1157.cpp
+++ [07]String/1157.cpp
@@ -3,25 +3,51 @@
 #include <vector>
 using namespace std;
 
-string toUpper(string orgstr) {
+vector<int> abc_cnt(26, 0);
+
+void CountAlphabet(string orgstr) {
   
   for(int i = 0; i < orgstr.length(); i++) {
+    
     if(orgstr.at(i) >= 97 && orgstr.at(i) <= 122) { // 소문자일 경우
       orgstr.at(i) -= 32; // 대문자로 변경
     }
+    
+    abc_cnt[orgstr.at(i) - 65]++; // 해당 알파벳에 카운트
   }
-  
-  return orgstr;
 }
 
+void getMax
+
 int main(void) {
+  // Input String
   string str;
   cin >> str;
   
-  // 다 대문자로 바꿔버릴까
-  string resstr;
-  resstr = toUpper(str);
+  // Count Alphabet
+  CountAlphabet(str);
   
-  cout << resstr;
+  // Find biggest
+  int mx = 0; int mx_idx = 0;
+  int qstmask = 0;
+  int samecnt = 0;
+  
+  for(int i = 0; i < abc_cnt.size(); i++) {
+    if(mx < abc_cnt[i]) {
+      mx = abc_cnt[i];
+      mx_idx = i;
+    }
+  }
+  
+  // Check biggest same
+  for(int i = 0; i < abc_cnt.size(); i++) {
+    if(mx == abc_cnt[i] && abc_cnt[i] > 0)
+      samecnt++;
+  }
+  
+  if(samecnt >= 2)
+    cout << "?" << "\n";
+  else
+    cout << (char)(mx_idx +65) << "\n";
   
 }
