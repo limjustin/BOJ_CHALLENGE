9ebe363d07b5752e63ff2c5e34ece95eec837acc
limjustin <limjyjustin@gmail.com>
2021-06-06 07:03:02 +0000

    20210606

--- [08]Math_01/10757.cpp
+++ [08]Math_01/10757.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 #include <vector>
@@ -47,4 +48,72 @@ int main(void) {
     cout << v[i];
   
   cout << "\n";
+}
+*/
+// 일단 정수로 받는 것은 말이 안댐
+// 근데 더할 때 인덱스가 꼬이는 문제가 생김
+// str 두개를 반복문으로 돌리되, 먼저 끝나면 나머지는 그대로 더하기
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(void) {
+  // Input string
+  string str1;
+  string str2;
+  cin >> str1 >> str2;
+  
+  int i = str1.length() - 1;
+  int j = str2.length() - 1;
+  
+  vector<int> res;
+  
+  int sum = 0;
+  int isTen = 0;
+  
+  do {
+    sum = (str1.at(i) - 48) + (str2.at(j) - 48) + isTen;
+    
+    if(sum > 9) {
+      res.push_back(sum % 10);
+      cout << sum % 10;
+      isTen = 1;
+    }
+    else {
+      res.push_back(sum);
+      cout << sum;
+      isTen = 0;
+    }
+    
+    printf("get log");
+    
+    i--; j--;
+    
+  } while (i >= 0 || j >= 0);
+  
+  // if (i != 0) {
+  //   for(int k = i; k >= 0; k--) {
+  //     res.push_back(str1.at(k) - 48 + isTen);
+      
+  //     if(str1.at(k - 48) + isTen > 9)
+  //       isTen = 1;
+  //     else
+  //       isTen = 0;
+  //   }
+  // } else if (j != 0) {
+  //   for(int k = j; k >= 0; k--) {
+  //     res.push_back(str2.at(k) - 48 + isTen);
+          
+  //     if(str2.at(k - 48) + isTen > 9)
+  //       isTen = 1;
+  //     else
+  //       isTen = 0;
+  //   }
+  // }
+  
+  // for(int i = res.size(); i >= 0; i--)
+  //   cout << res.at(i);
+  
 }
