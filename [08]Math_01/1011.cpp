a2a3622d1310329100f92028544e8a9fb5944a07
limjustin <limjyjustin@gmail.com>
2021-07-18 08:03:28 +0000

    20210718

--- [08]Math_01/1011.cpp
+++ [08]Math_01/1011.cpp
@@ -1,3 +1,63 @@
+#include <cstdio>
+#include <cmath>
+
+void Centauri(int s, int e) {
+
+    long long num = e - s - 2;
+    long long total = 4;
+    for(long long i = 3; num < pow(2, 31); i++) {
+        if(num == 1 || num == 2) {
+            printf("%d \n", 3);
+            break;
+        } else if(num == 3 || num == 4) {
+            printf("%d \n", 4);
+            break;
+        } else if (num == 0) {
+            printf("%d \n", 2);
+            break;
+        } else if (num == -1) {
+            printf("%d \n", 1);
+            break;            
+        }
+        total += 2*i;
+        if(num <= total) {
+            if((total - 2*i + total)/2 >= num) {
+                printf("%lld \n", (2*i) - 1);
+                break;
+            } else {
+                printf("%lld \n", 2*i);
+                break;
+            }
+        }
+    }
+
+}
+
+int main(void) {
+
+    int testcase;
+    scanf("%d", &testcase);
+
+    long long s, e;
+    while(testcase--) {
+        scanf("%lld %lld", &s, &e);
+        Centauri(s, e);
+
+    }
+
+}
+
+/*
+시간 초과되는 문제들, 방정식이나 수열로 풀면 풀림
+그니까 수학으로 풀라는 뜻임
+상식적으로 2^31 되는 문제들을 어떻게 재귀로 돌릴 생각을 했음
+대부분 규칙이 있을 것이라고 생각하고 접근?!
+
+점화식으로 코드 짜는 연습도 해야할듯
+점화식 구하는 방법도 공부하고 (예전 수학?)
+
+*/
+
 // #include <cstdio>
 
 // int s, e;
@@ -43,58 +103,3 @@
 //         cnt=0;
 //     }
 // }
-
-// int total = 4
-// for(int i = 3; num < 2^31; i++) {
-//     total += 2*i;
-//     if(num > total) printf("%d \n", 2*(i-1) );
-// }
-
-#include <cstdio>
-#include <cmath>
-
-void Centauri(int s, int e) {
-
-    int num = e - s - 2;
-    int total = 4;
-    for(int i = 3; num < (int)pow(2, 31); i++) {
-        printf("log");
-        if(num == 1 || num == 2) {
-            printf("%d \n", 3);
-            break;
-        } else if(num == 3 || num == 4) {
-            printf("%d \n", 4);
-            break;
-        } else if (num == 0) {
-            printf("%d \n", 2);
-            break;
-        } else if (num == -1) {
-            printf("%d \n", 1);
-            break;            
-        }
-        total += 2*i;
-        if(num <= total) {
-            if((total - 2*i + total)/2 >= num) {
-                printf("%d \n", (2*i) - 1);
-                break;
-            } else {
-                printf("%d \n", 2*i);
-                break;
-            }
-        }
-    }
-
-}
-
-int main(void) {
-
-    int testcase;
-    scanf("%d", &testcase);
-
-    int s, e;
-    while(testcase--) {
-        scanf("%d %d", &s, &e);
-        Centauri(s, e);
-    }
-
-}
