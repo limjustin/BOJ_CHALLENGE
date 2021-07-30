07dfa6bd84441c07089ca2c4c692757da4ecc5b8
limjustin <limjyjustin@gmail.com>
2021-07-30 04:29:01 +0000

    Re 20210730

--- [09]Math_02/1929.cpp
+++ [09]Math_02/1929.cpp
@@ -1,33 +1,23 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
-#define MAX 1000001
+#define MAX 1000000
 using namespace std;
 
 int main(void) {
-    int M, N;
-    scanf("%d %d", &M, &N);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
 
-    vector<bool> num(MAX, true);
+    // Get eratos
+    vector<bool> eratos(MAX + 1, true);
+    eratos[1] = false;
+
+    for(int num = 2; num <= MAX; num++) {
+        if(!eratos[num])
+            continue;
+
+        for(int idx = 2; idx <= MAX/num; idx++)
+            eratos[num * idx] = false;
+    }
+
     
-    for(int A = 2; A <= N; A++) {
-        if(num[A])
-            for(int i = 2; i < MAX / A; i++) {
-                num[i * A] = false;
-            }
-    }
-
-    for(int i = M; i <= N; i++) {
-        if(num[i] && i != 1)
-            printf("%d \n", i);
-    }
 }
-/*
-<매우 고전했던 문제>
-- 실수 : 분명히 for문을 두 번 돌리면 안되는 시간복잡도였거든... (시간 초과)
-    ㄴ 에라토스 만들어놓고 또 소수 판별 알고리즘 돌려서 시간 초과 만들어 냈다!
-
-- 해결 : 소수 판별 알고리즘을 사용하지 않고도 소수를 판별할 수 있었다! (에라토스테네스의 채)
-    ㄴ 일단 한번 걸러서 소수만 남겨두고, 해당 범위 내에 해당하는 소수들만 뽑아내면 되었다!
-
-- 결론 : 이 알고리즘을 꼭 써야 되었다는 강박관념 버리기
-*/
