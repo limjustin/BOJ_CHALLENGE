aad011bd2ac5235c00fcc50762ad2238ad8ac6f6
limjustin <limjyjustin@gmail.com>
2021-08-03 11:50:24 +0000

    Re 20210803

--- [08]Math_01/1011.cpp
+++ [08]Math_01/1011.cpp
@@ -1,105 +1,45 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
 #include <cmath>
+using namespace std;
 
-void Centauri(int s, int e) {
+int Centauri(int num) {
+    int len; int res;
 
-    long long num = e - s - 2;
-    long long total = 4;
-    for(long long i = 3; num < pow(2, 31); i++) {
-        if(num == 1 || num == 2) {
-            printf("%d \n", 3);
+    // 해당하는 일반항 인덱스를 구하기
+    for(int n = 1; n*n + n < pow(2, 31); n++) {
+        if(num <= n*n + n) {
+            len = n;
             break;
-        } else if(num == 3 || num == 4) {
-            printf("%d \n", 4);
-            break;
-        } else if (num == 0) {
-            printf("%d \n", 2);
-            break;
-        } else if (num == -1) {
-            printf("%d \n", 1);
-            break;            
-        }
-        total += 2*i;
-        if(num <= total) {
-            if((total - 2*i + total)/2 >= num) {
-                printf("%lld \n", (2*i) - 1);
-                break;
-            } else {
-                printf("%lld \n", 2*i);
-                break;
-            }
         }
     }
 
+    // 위 처럼 하면 안 되고, 이분탐색 알고리즘을 짜볼까?
+    
+
+    // 더 가까운 쪽을 판별하기
+    if((len*len+len - num) > (num - ((len-1)*(len-1)+(len-1) + 1))) {
+        res = 2 * len - 1;   
+    } else {
+        res = 2 * len;
+    }
+
+    return res;
 }
 
 int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
+    // 테스트 케이스 값 넣기
     int testcase;
-    scanf("%d", &testcase);
+    cin >> testcase;
 
-    long long s, e;
+    // 각 테스트케이스에 대한 답 추출
+    int x, y;
+
     while(testcase--) {
-        scanf("%lld %lld", &s, &e);
-        Centauri(s, e);
-
+        cin >> x >> y;
+        cout << Centauri(y - x) << "\n";
     }
-
 }
-
-/*
-시간 초과되는 문제들, 방정식이나 수열로 풀면 풀림
-그니까 수학으로 풀라는 뜻임
-상식적으로 2^31 되는 문제들을 어떻게 재귀로 돌릴 생각을 했음
-대부분 규칙이 있을 것이라고 생각하고 접근?!
-
-점화식으로 코드 짜는 연습도 해야할듯
-점화식 구하는 방법도 공부하고 (예전 수학?)
-
-*/
-
-// #include <cstdio>
-
-// int s, e;
-// int cnt = 0;
-
-// int Centauri(int len, int location) {
-
-//     // printf("Centauri(%d, %d) \n", len, location);
-    
-//     if(location == e-1 && (len == 1 || len == 2)) {
-//         return 1;
-//     } else if(location > e-1) {
-//         return 0;
-//     } else {
-//         if(Centauri(len+1, location+len+1)) {
-//             cnt++;
-//             return 1;
-//         } else if(Centauri(len, location+len)) {
-//             cnt++;
-//             return 1;
-//         } else if(Centauri(len-1, location+len-1)) {
-//             cnt++;
-//             return 1;
-//         }
-//         return 0;
-//     }
-
-//     return cnt;
-// }
-
-// int main(void) {
-//     int testcase;
-//     scanf("%d", &testcase);
-
-//     while(testcase--) {
-//         scanf("%d %d", &s, &e);
-//         if(e - s == 1) {
-//             printf("%d \n", 1);
-//         }
-//         else if(Centauri(1, s+1)) {
-//             printf("%d \n", cnt + 2);
-//         }
-//         cnt=0;
-//     }
-// }
