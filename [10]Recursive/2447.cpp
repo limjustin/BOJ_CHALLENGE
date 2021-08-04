42cfe537be0349c3a5a6b7547daa26d4df72e49d
limjustin <limjyjustin@gmail.com>
2021-08-04 07:34:05 +0000

    Re 20210804

--- [10]Recursive/2447.cpp
+++ [10]Recursive/2447.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <vector>
-// #define MAX 6561
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// // #define MAX 6561
+// using namespace std;
 
-void Recursivestar(int N) {
+// void Recursivestar(int N) {
 
-    for(int i = 0; i < N; i++) {
+//     for(int i = 0; i < N; i++) {
 
-    }
+//     }
 
-    // 지정된 수가 저장된 배열을 돌려서 그 좌표에 해당하는 구역에 * 대신 공백을 찍는 것이지!!
-    for(int x = 0; x < N; x++) { // 
+//     // 지정된 수가 저장된 배열을 돌려서 그 좌표에 해당하는 구역에 * 대신 공백을 찍는 것이지!!
+//     for(int x = 0; x < N; x++) { // 
 
-        for(int y = 0; y < N; y++) { // y 좌표
+//         for(int y = 0; y < N; y++) { // y 좌표
 
-        }
+//         }
 
-    }
-}
+//     }
+// }
 
-int main(void) {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL); cout.tie(NULL);
+// int main(void) {
+//     // ios_base::sync_with_stdio(false);
+//     // cin.tie(NULL); cout.tie(NULL);
 
-    // 3의 배수 N 입력 받기
-    int N;
-    cin >> N;
+//     // 3의 배수 N 입력 받기
+//     int N;
+//     cin >> N;
 
-    vector< vector<char> > allStar;
-    vector<char> v;
+//     vector< vector<char> > allStar;
+//     vector<char> v;
 
-    for(int x = 0; x < N; x++) {
-        for(int y = 0; y < N; y++) {
-            v.push_back('*');
-        }
-        allStar.push_back(v);
-    }
+//     for(int x = 0; x < N; x++) {
+//         for(int y = 0; y < N; y++) {
+//             v.push_back('*');
+//         }
+//         allStar.push_back(v);
+//     }
 
-    // 함수 결과 출력
-    Recursivestar(N);
-}
+//     // 함수 결과 출력
+//     Recursivestar(N);
+// }
 
 // ***
 // * * (1,1)
@@ -60,3 +60,32 @@ int main(void) {
 
 // 아니면 숫자를 기억하게 하는거지 (1,3,4,5,7 등)
 // 그 숫자만 for문을 돌면 되구나! (일단 다 별을 박아두고!!)
+
+#include <cstdio>
+
+void Recursivestar(int x, int y, int num) {
+
+    // 공백 출력 상황
+    if((x/num)%3 == 1 && (y/num)%3 == 1)
+        printf(" ");
+    
+    // 별 출력 상황
+    else {
+        if ((x/num) == 0 || (y/num) == 0)
+            printf("*");
+        else  // 어떻게든 끝까지 따라가서 나누어야 했네
+            Recursivestar(x, y, num*3);
+    }
+}
+
+int main(void) {
+    int num;
+    scanf("%d", &num);
+
+    for(int x = 0; x < num; x++) {
+        for(int y = 0; y < num; y++) {
+            Recursivestar(x, y, 1);
+        }
+        printf("\n");
+    }
+}
