e38373ec0368d5e2d29caab37cba9d0688f22547
limjustin <limjyjustin@gmail.com>
2021-09-13 07:25:32 +0000

    20210913

--- [12]Sorting/1181.cpp
+++ [12]Sorting/1181.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <map>
 #include <algorithm>
 using namespace std;
 
 bool compare(pair<string, int> p1, pair<string, int> p2) {
 
     if(p1.second == p2.second && p1.first.compare(p2.first) < 0)
-        return p1.first > p2.first;
+        return p1.first < p2.first;
 
     else if(p1.second == p2.second && p1.first.compare(p2.first) > 0)
-        return p1.first < p2.first;    
+        return p1.first > p2.first;    
         
     else
         return p1.second < p2.second;
@@ -25,7 +24,6 @@ int main(void) {
     cin >> N;
 
     string str;
-    map<string, int> m;
     vector< pair<string, int> > words;
 
     for(int i = 0; i < N; i++) {
@@ -33,15 +31,28 @@ int main(void) {
         words.push_back({str, str.length()}); // key가 중복되지 않게
     }
 
+    string str1 = "wait";
+    string str2 = "wont";
+    cout << str2.compare(str1) << "\n";
+
     // cout << "Before Sort \n";
     // for(auto iter = words.begin(); iter != words.end(); iter++)
     //     cout << iter->first << "\n";
 
+    // 내 방법
+    // 1. 일단 문자열 비교를 하고 싶은데...
+
     sort(words.begin(), words.end(), compare);
 
-    words.erase(unique(words.begin(), words.end()), words.end());
+    // words.erase(unique(words.begin(), words.end()), words.end());
 
     // cout << "After Sort \n";
-    for(auto iter = words.begin(); iter != words.end(); iter++)
-        cout << iter->first << "\n";
+    // for(auto iter = words.begin(); iter != words.end(); iter++)
+    //     cout << iter->first << "\n";
 }
+
+// 문자열 비교하기 (compare 함수)
+
+// vector에서 중복원소 제거하기
+
+// stable_sort가 뭔데?
