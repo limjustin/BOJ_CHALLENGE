137ed4ed0795febae6f872e12e626b496c06365f
limjustin <limjyjustin@gmail.com>
2021-09-24 08:01:08 +0000

    20210924

--- [12]Sorting/10814.cpp
+++ [12]Sorting/10814.cpp
@@ -4,10 +4,7 @@
 using namespace std;
 
 bool compare(pair<int, string> p1, pair<int, string> p2) {
-    if(p1.first == p2.first)
-        continue;
-    else
-        return p1.first < p2.first;
+    return p1.first < p2.first;
 }
 
 int main(void) {
